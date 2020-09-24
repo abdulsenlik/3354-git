c788c3ca153a549e0f3a3e81a4322bd0bc1e6cba
abdulsenlik <71569381+abdulsenlik@users.noreply.github.com>
2020-09-24 16:01:43 -0500

    Update Main.java

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -4,10 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+          
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
             System.err.println("Please provide two integers to add");
+        }
+        try {
+            int result = addArguments(args);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.err.println("Invalid");
+        }
+        try {
+            int result = addArguments(args);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.err.println("Invalid");
         }
     }
 
